cae3aa47c4668ad3bb7d29873b517b607f2eade4
Pawel Muchowski <pmuchowski91@proton.me>
2025-06-26 13:18:38 +0200

    Create Contact page

---
+++ src/app/(frontend)/contact/page.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FacebookIcon, GithubIcon, InstagramIcon, MailIcon, PhoneIcon } from 'lucide-react'
+import Page from '@/components/Page'
+
+const ContactPage: React.FC = () => (
+  <Page title="Zostań z nami w kontakcie">
+    <div className="flex flex-col items-center justify-center gap-2">
+      <div className="flex items-center justify-center">
+        <div className="px-4">
+          <PhoneIcon className="inline mr-1 text-contact-icons" />
+          <span>+ 48 000-111-222</span>
+        </div>
+        <div className="px-4">
+          <MailIcon className="inline mr-1 text-contact-icons" />
+          <span>horyzont@xyz.com</span>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="px-4">
+          <FacebookIcon className="inline mr-1 text-contact-icons" />
+          <span>@fundacja.horyzont</span>
+        </div>
+        <div className="px-4">
+          <InstagramIcon className="inline mr-1 text-contact-icons" />
+          <span>@fundacja.horyzont</span>
+        </div>
+        <div className="px-4">
+          <GithubIcon className="inline mr-1 text-contact-icons" />
+          <span>@fundacja.horyzont</span>
+        </div>
+      </div>
+    </div>
+  </Page>
+)
+
+export default ContactPage
